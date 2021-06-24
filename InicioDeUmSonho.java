b424f01464f700e771594f97d52a62650b0199bd
Luis Felipe <luisfelipesdn12@gmail.com>
2021-06-24 12:08:46 -0300

    Format file
    (just to test if I can commit)

--- InicioDeUmSonho.java
+++ InicioDeUmSonho.java
@@ -3,40 +3,51 @@ package iniciodeumsonho;
 import javax.swing.JOptionPane;
 
 public class InicioDeUmSonho {
-
     public static void main(String[] args) {
-
-        JOptionPane.showMessageDialog(null, "Boas vindas ao Sistema do Restaurante Flamboyant!", "Mensagem",
-                JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(
+            null,
+            "Boas vindas ao Sistema do Restaurante Flamboyant!",
+            "Mensagem",
+            JOptionPane.INFORMATION_MESSAGE
+        );
         String menuInicial = "";
 
-        while (!(menuInicial.equals("0") || menuInicial.equals("1") || menuInicial.equals("2")
-                || menuInicial.equals("3") || menuInicial.equals("4"))) {
-            menuInicial = JOptionPane
-                    .showInputDialog(
-                            null, "Digite a opção desejada \n" + "1- Cadastrar \n" + "2- Consultar \n"
-                            + "3- Alterar  \n" + "4- Excluir  \n" + "0- Finalizar",
-                            "Menu", JOptionPane.QUESTION_MESSAGE);
-            // JOptionPane.showMessageDialog(null, "A opção escolhida é: " +
-            // menuInicial);//acho desnecessário
+        while (!(
+            menuInicial.equals("0")
+            || menuInicial.equals("1")
+            || menuInicial.equals("2")
+            || menuInicial.equals("3")
+            || menuInicial.equals("4")
+        )) {
+            menuInicial = JOptionPane.showInputDialog(
+                null,
+                "Digite a opção desejada\n"
+                + "1- Cadastrar\n"
+                + "2- Consultar\n"
+                + "3- Alterar\n"
+                + "4- Excluir\n"
+                + "0- Finalizar",
+                "Menu",
+                JOptionPane.QUESTION_MESSAGE
+            );
         }
-        switch (menuInicial) { 
+
+        switch (menuInicial) {
             case "1":
-                 Produto.cadastraProduto();// caso seja 1 mostre:.. depois "quebre"//
+                // caso seja 1 mostre:.. depois "quebre"//
+                Produto.cadastraProduto();
                 break;
             case "2":
-            Produto.consultaProduto();
+                Produto.consultaProduto();
                 break;
             case "3":
-              Produto.consultaParaAlterar();
+                Produto.consultaParaAlterar();
                 break;
             case "4":
-               Produto.Excluir();
+                Produto.Excluir();
                 break;
             default:
                 break;
         }
-
     }
-
 }
