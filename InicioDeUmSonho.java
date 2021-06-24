7893f35e82e7b3e58e4f521f3201a27ac142de5a
valencprado <85965282+valencprado@users.noreply.github.com>
2021-06-24 12:01:44 -0300

    Add Cadastrar, Consultar, Alterar

--- InicioDeUmSonho.java
+++ InicioDeUmSonho.java
@@ -6,39 +6,37 @@ public class InicioDeUmSonho {
 
     public static void main(String[] args) {
 
-        JOptionPane.showMessageDialog(null, "Boas vindas ao Sistema do Restaurante Flamboyant!", "Mensagem", JOptionPane.INFORMATION_MESSAGE);
-
+        JOptionPane.showMessageDialog(null, "Boas vindas ao Sistema do Restaurante Flamboyant!", "Mensagem",
+                JOptionPane.INFORMATION_MESSAGE);
         String menuInicial = "";
-        while (!(menuInicial.equals("0")
-                || menuInicial.equals("1")
-                || menuInicial.equals("2")
-                || menuInicial.equals("3")
-                || menuInicial.equals("4"))) {
-            menuInicial = JOptionPane.showInputDialog(null, "Digite a opção desejada \n"
-                    + "1- Cadastrar \n"
-                    + "2- Consultar \n"
-                    + "3- Alterar  \n"
-                    + "4- Excluir  \n"
-                    + "0- Finalizar");
-           // JOptionPane.showMessageDialog(null, "A opção escolhida é: " + menuInicial);
+
+        while (!(menuInicial.equals("0") || menuInicial.equals("1") || menuInicial.equals("2")
+                || menuInicial.equals("3") || menuInicial.equals("4"))) {
+            menuInicial = JOptionPane
+                    .showInputDialog(
+                            null, "Digite a opção desejada \n" + "1- Cadastrar \n" + "2- Consultar \n"
+                            + "3- Alterar  \n" + "4- Excluir  \n" + "0- Finalizar",
+                            "Menu", JOptionPane.QUESTION_MESSAGE);
+            // JOptionPane.showMessageDialog(null, "A opção escolhida é: " +
+            // menuInicial);//acho desnecessário
         }
-        switch (menuInicial) {
+        switch (menuInicial) { 
             case "1":
-                JOptionPane.showMessageDialog(null, "A opção escolhida é: Cadastrar");
+                 Produto.cadastraProduto();// caso seja 1 mostre:.. depois "quebre"//
                 break;
             case "2":
-                JOptionPane.showMessageDialog(null, "A opção escolhida é: Consultar");
+            Produto.consultaProduto();
                 break;
             case "3":
-                JOptionPane.showMessageDialog(null, "A opção escolhida é: Alterar");
+              Produto.consultaParaAlterar();
                 break;
             case "4":
-                JOptionPane.showMessageDialog(null, "A opção escolhida é: Excluir");
+               Produto.Excluir();
                 break;
             default:
                 break;
-
         }
+
     }
 
 }
